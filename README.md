70514e1379c28411f00c9e034691e1ae53fe56bd
Kamil Kowalewski <szamil24@gmail.com>
2021-10-09 17:49:14 +0200

    Add partially done impl of task2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EDI
+
+## Packages Installation
+Go to directory with selected task and run below command
+```
+pip install -r requirements.txt
+```
--- Task2/main.py
+++ Task2/main.py
@@ -1,7 +1,14 @@
+import os
 import subprocess
 import sys
+from argparse import ArgumentParser, Namespace
+
+import html2text
 
 """
+    How to run:
+        python main.py --plain -f data/abc.html
+        python main.py --arff -f data/abc.txt
 """
 
 
@@ -9,10 +16,49 @@ import sys
 
 # MAIN ----------------------------------------------------------------------- #
 def main() -> None:
+    args = prepare_args()
+    plain = args.plain
+    arff = args.arff
+    filename = args.filename
+
+    if plain:
+        convert_html_to_plain(filename)
+
+    if arff:
+        convert_plain_text_to_arff(filename)
+
     display_finish()
 
 
 # DEF ------------------------------------------------------------------------ #
+def convert_html_to_plain(filename: str) -> None:
+    with open(filename) as file:
+        plain_text = html2text.html2text(file.read())
+
+    trimmed_filename = os.path.splitext(os.path.basename(filename))[0]
+    with open(trimmed_filename, "w", encoding="utf-8") as file:
+        file.write(plain_text)
+
+
+def convert_plain_text_to_arff(filename: str) -> None:
+    pass
+
+
+def prepare_args() -> Namespace:
+    arg_parser = ArgumentParser()
+
+    arg_parser.add_argument(
+        "--plain", default=False, action="store_true", help="Convert HTML to plain text"
+    )
+    arg_parser.add_argument(
+        "--arff", default=False, action="store_true", help="Convert plain text to arff"
+    )
+    arg_parser.add_argument(
+        "-f", "--filename", type=str, help="HTML or plain text filename"
+    )
+
+    return arg_parser.parse_args()
+
 
 # UTIL ----------------------------------------------------------------------- #
 def check_types_check_style() -> None:
