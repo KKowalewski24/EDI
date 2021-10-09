56ac1db647cb723061ed9c506a12695c528eacdc
Kamil Kowalewski <szamil24@gmail.com>
2021-10-09 12:12:12 +0200

    Add partially working script

--- Task1/main.py
+++ Task1/main.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
 import sys
-from datetime import datetime
-from typing import Dict
+from datetime import datetime, timedelta
+from typing import Dict, Tuple
 
 import arff
 import pandas as pd
 from nameof import nameof
+from tqdm import tqdm
 
 """
 """
@@ -19,20 +20,25 @@ ARFF = "arff"
 SECONDS_IN_MINUTE = 60
 FIXED_SESSION_DURATION = 10 * SECONDS_IN_MINUTE
 POPULAR_SITE_FRACTION = 0.5
+ROWS_NUMBER_TO_READ = 50000
 
 
 # MAIN ----------------------------------------------------------------------- #
 def main() -> None:
     create_directory(OUTPUT_DIR)
-    df: pd.DataFrame = filter_logs(pd.read_csv(LOGS_PATH))
-    unique_users: pd.DataFrame = df["host"].unique()
-    popular_sites: pd.DataFrame = get_popular_sites(df)
+    print("Preparing logs ...")
+    df: pd.DataFrame = prepare_logs(filter_logs(pd.read_csv(LOGS_PATH, nrows=ROWS_NUMBER_TO_READ)))
+    print("Preparing sessions and users ...")
+    sessions, users = prepare_sessions_and_users(df)
+    # print(sessions)
+    # print(users)
+    # sessions_pages = sessions.drop(columns=['duration', 'requests_count', 'avg_request_duration'])
+    # sessions_numeric = sessions[['duration', 'requests_count', 'avg_request_duration']]
+    # users_pages = users.drop(columns=['requests_count'])
 
-    sessions = []
-    users = []
-
-    for data_frame in []:
-        save_df_to_csv_and_arff(data_frame)
+    print("Saving processed data to files ...")
+    for data_frame, label in zip([sessions, users], [nameof(sessions), nameof(users)]):
+        save_df_to_csv_and_arff(data_frame, label)
 
     display_finish()
 
@@ -42,29 +48,85 @@ def filter_logs(df: pd.DataFrame) -> pd.DataFrame:
     return df.loc[
         (df["method"] == "GET")
         & (df["response"] == 200)
-        & (df['url'].str.contains('.jpg|.gif|.bmp|.xbm|.png|.jpeg') == False)
+        & (~df["url"].str.contains(".jpg|.gif|.bmp|.xbm|.png|.jpeg"))
         ]
 
 
+def prepare_logs(df: pd.DataFrame) -> pd.DataFrame:
+    df['time'] = pd.to_datetime(df['time'])
+    return df
+
+
 def get_popular_sites(df: pd.DataFrame) -> pd.DataFrame:
-    sites_counts = df['url'].value_counts().rename('count')
-    sites_percents = (df['url'].value_counts(normalize=True) * 100).rename('percent')
+    sites_counts = df["url"].value_counts().rename("count")
+    sites_percents = (df['url'].value_counts(normalize=True) * 100).rename("percent")
 
     sites = pd.concat([sites_counts, sites_percents], axis=1)
-    sites = sites.reset_index().rename({'index': 'url'}, axis=1)
+    sites = sites.reset_index().rename({"index": "url"}, axis=1)
 
-    return sites[sites['percent'] > POPULAR_SITE_FRACTION]
+    return sites[sites["percent"] > POPULAR_SITE_FRACTION]
 
 
 def get_empty_popular_sites_dictionary(popular_sites: pd.DataFrame) -> Dict[str, bool]:
     return {b: False for b in popular_sites['url']}
 
 
-def save_df_to_csv_and_arff(df: pd.DataFrame) -> None:
-    collection_name = nameof(df)
-    df.to_csv(get_filename(collection_name, CSV), index=False)
+def prepare_sessions_and_users(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    unique_users: pd.DataFrame = df["host"].unique()
+    popular_sites: pd.DataFrame = get_popular_sites(df)
+
+    sessions = []
+    users = []
+
+    for i in tqdm(range(len(unique_users))):
+        requests = pd.DataFrame(df[df["host"] == unique_users[i]])
+
+        session_start = requests.iloc[0]["time"]
+        session_end = session_start
+        session_visited_sites = get_empty_popular_sites_dictionary(popular_sites)
+        user_visited_sites = get_empty_popular_sites_dictionary(popular_sites)
+        session_requests_count = 0
+        user_requests_count = 0
+
+        for index, request in requests.iterrows():
+            if request["time"] - session_end > timedelta(seconds=FIXED_SESSION_DURATION):
+                if session_requests_count > 1:
+                    session_duration = (session_end - session_start).total_seconds()
+                    avg_request_duration = session_duration / (session_requests_count - 1)
+                    sessions.append(
+                        {
+                            **{
+                                "duration": session_duration,
+                                "requests_count": session_requests_count,
+                                "avg_request_duration": avg_request_duration
+                            },
+                            **session_visited_sites
+                        }
+                    )
+
+                session_start = request["time"]
+                session_visited_sites = get_empty_popular_sites_dictionary(popular_sites)
+                session_requests_count = 0
+
+            if request["url"] in session_visited_sites:
+                session_visited_sites[request["url"]] = True
+                user_visited_sites[request["url"]] = True
+
+            session_requests_count += 1
+            user_requests_count += 1
+            session_end = request["time"]
+
+        users.append(
+            {**{'requests_count': user_requests_count}, **session_visited_sites}
+        )
+
+    return pd.DataFrame(sessions), pd.DataFrame(users)
+
+
+def save_df_to_csv_and_arff(df: pd.DataFrame, collection_name: str) -> None:
+    df.to_csv(OUTPUT_DIR + get_filename(collection_name, CSV), index=False)
     arff.dump(
-        get_filename(collection_name, ARFF), df.values,
+        OUTPUT_DIR + get_filename(collection_name, ARFF), df.values,
         relation=collection_name, names=df.columns
     )
 
