ef394eeb769cd03c358ec022228dc179e7d21a63
Kamil Kowalewski <szamil24@gmail.com>
2021-10-13 21:35:34 +0200

    Small fix

--- Task2/main.py
+++ Task2/main.py
@@ -70,7 +70,7 @@ def get_filename_from_path(filename: str) -> str:
 
 
 def prepare_filename(name: str, extension: str, add_date: bool = True) -> str:
-    return (name + "-" + (datetime.now().strftime("%H%M%S") if add_date else "")
+    return (name + ("-" + datetime.now().strftime("%H%M%S") if add_date else "")
             + extension).replace(" ", "")
 
 
