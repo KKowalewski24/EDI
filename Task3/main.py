1d1427f0c558eb2e0d5e83d2f3b221e60b9820c3
Kamil Kowalewski <szamil24@gmail.com>
2021-11-01 21:16:01 +0100

    Add generating user

--- Task3/main.py
+++ Task3/main.py
@@ -1,8 +1,12 @@
 import os
+import random
 import subprocess
 import sys
 from argparse import ArgumentParser, Namespace
 from datetime import datetime
+from typing import List
+
+from sklearn.metrics import jaccard_score
 
 """
     How to run:
@@ -11,17 +15,35 @@ from datetime import datetime
 
 # VAR ------------------------------------------------------------------------ #
 OUTPUT_DIR: str = "output/"
+# TODO TEST AND MAYBE CHANGE
+CONSTANT_USER: List[bool] = [
+    True, False, False, False, False, True, False, True, True, True, False, False, False,
+    False, True, True, False, False, True, False, False, True, True, False, False, False,
+    False, True, False, True
+]
 
 
 # MAIN ----------------------------------------------------------------------- #
 def main() -> None:
     args = prepare_args()
     filepath = args.filepath
+    is_random_user = args.random_user
+    random_user_pages_number = args.random_user_pages_number
+
+    print("Preparing user ...")
+    user: List[bool] = get_user(is_random_user, random_user_pages_number)
 
     display_finish()
 
 
 # DEF ------------------------------------------------------------------------ #
+def get_user(is_random_user: bool, random_user_pages_number: int) -> List[bool]:
+    if is_random_user:
+        return [random.choice([True, False]) for _ in range(random_user_pages_number)]
+
+    return CONSTANT_USER
+
+
 def get_filename_from_path(filepath: str) -> str:
     return os.path.splitext(os.path.basename(filepath))[0]
 
@@ -36,6 +58,13 @@ def prepare_args() -> Namespace:
 
     arg_parser.add_argument(
         "-f", "--filepath", required=True, type=str, help="Clusters filepath"
+    )
+    arg_parser.add_argument(
+        "-ru", "--random_user", default=False, action="store_true", help="Generate random user"
+    )
+    arg_parser.add_argument(
+        "-pn", "--random_user_pages_number", type=int,
+        help="Number of pages for randomly generated user"
     )
 
     return arg_parser.parse_args()
